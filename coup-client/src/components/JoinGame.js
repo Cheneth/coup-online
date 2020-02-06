9e3f9bf9e5fc116bd327f2b84e0561778ac3e085
Ethan <ethan190c@gmail.com>
2020-02-05 23:49:44 -0500

    hehe

---
+++ coup-client/src/components/JoinGame.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+export default class JoinGame extends Component {
+
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+            name: '',
+            roomCode: ''
+        }
+    }
+    
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
--- server/index.js
+++ server/index.js
@@ -26,6 +26,7 @@ gameSocket.on('connection', (socket) => {
         "_id" : socket.id,
         "player": '',
         "socket_room": `${socket.id}`,
+<<<<<<< Updated upstream
     });
     const index = players.length - 1;
 
@@ -51,7 +52,43 @@ gameSocket.on('connection', (socket) => {
     setInterval(() => {
         socket.in(players[index].socket_room).emit("time", `player${players[index].player}-${moment().format()}`);
         // console.log('emit');
+=======
+        "isReady": false
+    })
+    console.log(`player ${players.length} has connected`);
+    socket.join(socket.id);
+    console.log('socket joined ' + socket.id)
+    const index = players.length-1
+
+    const updatePlayerList = () => {
+        let updatedPlayers = players.map(x => {
+            return {name: x.player, socketID: x._id, isReady: x.isReady}
+        }).filter(x => x.name != '')
+        console.log(updatedPlayers)
+        gameSocket.emit('playerJoined', updatedPlayers) 
+    }
+
+    setInterval(() => {
+        gameSocket.to(socket.id).emit("time", `${players[index].player}-${moment().format()}`)
+        console.log('emit')
+>>>>>>> Stashed changes
     }, 1000);
+
+    socket.on('setName', (name) => { //when client joins, it will immediately set its name
+        if(!players.map(x => x.player).includes(name)){
+            players[index].player = name
+            console.log(players[index])
+            updatePlayerList()
+            socket.in(players[index].socket_room).emit("joinSuccess", players[index]._id)
+        } else {
+            socket.in(players[index].socket_room).emit("joinFailed", 'name_taken')
+        }  
+    })
+    socket.on('setReady', (isReady) => { //when client is ready, they will update this
+        console.log(`${players[index].player} is ready`);
+        players[index].isReady = isReady;
+    })
+   
     socket.on('disconnect', () => {
         console.log('disconnected: ' + socket.id);
         players.map((x,index) => {

df226c1942b95f3128bca2e5f80a01a2ffce6369
Ethan <ethan190c@gmail.com>
2020-03-14 13:26:12 -0400

    added coin deduction for assassinate and coup

--- coup-client/src/components/game/ActionDecision.js
+++ coup-client/src/components/game/ActionDecision.js
@@ -9,7 +9,8 @@ export default class ActionDecision extends Component {
             isDecisionMade: false,
             decision: '',
             isPickingTarget: false,
-            targetAction: ''
+            targetAction: '',
+            actionError: ''
         }
     }
 
@@ -25,6 +26,25 @@ export default class ActionDecision extends Component {
         
         this.props.socket.emit('g-actionDecision', res)
         this.props.doneAction();
+    }
+
+    deductCoins = (action) => {
+        console.log(this.props.money, action)
+        if(action === 'assassinate') {
+            if(this.props.money >= 3) {
+                this.props.deductCoins(3);
+                this.pickingTarget('assassinate');
+            } else {
+                this.setState({ actionError: 'Not enough coins to assassinate!'})
+            }
+        } else if(action === 'coup') {
+            if(this.props.money >= 7) {
+                this.props.deductCoins(7);
+                this.pickingTarget('coup');
+            } else {
+                this.setState({ actionError: 'Not enough coins to coup!'})
+            }
+        }
     }
 
     pickingTarget = (action) => {
@@ -46,12 +66,13 @@ export default class ActionDecision extends Component {
            controls = ( 
            <>
                 <button onClick={() => this.chooseAction('income')}>Income</button>
-                <button onClick={() => this.pickingTarget('coup')}>Coup</button>
+                <button onClick={() => this.deductCoins('coup')}>Coup</button>
                 <button onClick={() => this.chooseAction('foreign_aid')}>Foreign Aid</button>
                 <button onClick={() => this.pickingTarget('steal')}>Steal</button>
-                <button onClick={() => this.pickingTarget('assassinate')}>Assassinate</button>
+                <button onClick={() => this.deductCoins('assassinate')}>Assassinate</button>
                 <button onClick={() => this.chooseAction('tax')}>Tax</button>
                 <button onClick={() => this.chooseAction('exchange')}>Exchange</button>
+                <p>{this.state.actionError}</p>
            </> 
            )
         }

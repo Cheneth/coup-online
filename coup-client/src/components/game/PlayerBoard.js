5639d85dcd266a0f36499663c55483d77cdeaa8f
Ethan <ethan190c@gmail.com>
2020-03-22 12:35:09 -0400

    added player colors

--- coup-client/src/components/game/PlayerBoard.js
+++ coup-client/src/components/game/PlayerBoard.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import './PlayerBoardStyles.css'
 
 export default function PlayerBoard(props) {
     let boardItems = null
     if(props.players.length > 1) {
         boardItems = props.players.map((player, index) =>
-            <span key={index} style={{display: "inline-block", marginRight: "10px", marginLeft: "10px"}}>
-                <p>{player.name}</p>
+            <span className="PlayerBoardItem" style={{ backgroundColor: `${player.color}` }} key={index}>
+                <h2>{player.name}</h2>
                 <p>Coins: {player.money}</p>
                 <p>Influences: {player.influences.length}</p>
                 {/* <p>{player.influences.join(', ')}</p> */}

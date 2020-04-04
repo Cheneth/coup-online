a62c612ff5af1aa568e436224d37818c541b6e71
Ethan <ethan190c@gmail.com>
2020-04-03 22:21:52 -0400

    switched baseUrl

--- coup-client/src/components/JoinGame.js
+++ coup-client/src/components/JoinGame.js
@@ -3,8 +3,8 @@ import io from "socket.io-client";
 import Coup from './game/Coup';
 
 const axios = require('axios');
-const baseUrl = 'http://localhost:8000'
-// const baseUrl = 'https://rocky-stream-49978.herokuapp.com'
+// const baseUrl = 'http://localhost:8000'
+const baseUrl = 'https://rocky-stream-49978.herokuapp.com'
 
 
 export default class JoinGame extends Component {

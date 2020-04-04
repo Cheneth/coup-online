06534952f8775baf5d9dbfcf791b463b63e1c943
Ethan <ethan190c@gmail.com>
2020-04-04 12:28:37 -0400

    fixed cheatsheet

--- coup-client/src/components/game/Coup.js
+++ coup-client/src/components/game/Coup.js
@@ -315,21 +315,6 @@ export default class Coup extends Component {
            
             <div className="RulesContainer">
                 <div className="RulesContent">
-                    <h2>Influences</h2>
-                    <h3>Captain</h3>
-                    <p><b id="captain-color">STEAL</b>: Steal 2 coins from a target. Blockable by <hl id="captain-color">Captain</hl> or <hl id="ambassador-color">Ambassador</hl>.</p>
-                    <h3>Assassin</h3>
-                    <p><b id="assassin-color">ASSASSINATE</b>: Pay 3 coins to choose a target to assassinate (target loses an influence). Blockable by <hl id="contessa-color">Contessa</hl>.</p>
-                    <h3>Duke</h3>
-                    <p><b id="duke-color">TAX</b>: Collect 3 coins from the treasury. Not blockable.</p>
-                    <h3>Ambassador</h3>
-                    <p><b id="ambassador-color">EXCHANGE</b>: Draw 2 influences into your hand and pick any 2 influences to put back. Not blockable.</p>
-                    <h3>Contessa</h3>
-                    <p><b id="contessa-color">BLOCK ASSASSINATION</b>: Can block assassinations. Not blockable.</p>
-                    <h3>Other Actions</h3>
-                    <p><b>INCOME</b>: Collect 1 coins from the treasury</p>
-                    <p><b>FOREIGN AID</b>: Collect 2 coins from the treasury. Blockable by <hl id="duke-color">Duke</hl></p>
-                    <p><b>COUP</b>: Pay 7 coins and choose a target to lose an influence. If a player starts their turn with 10 coins, they must Coup. Not Blockable.</p>
                     <h2>Rules</h2>
                     <p>On your turn, you may choose an action to play. The action you choose may or may not correspond to the influences that you possess. 
                         For the action that you choose, other players may potentially block or challenge it. </p>
@@ -344,6 +329,24 @@ export default class Coup extends Component {
                     <p>
                         If a player loses all their influences, they are out of the game. The last player standing wins!
                     </p>
+                    <p>
+                        At this time, if a player disconnects, the game must be recreated.
+                    </p>
+                    <h2>Influences</h2>
+                    <h3>Captain</h3>
+                    <p><b id="captain-color">STEAL</b>: Steal 2 coins from a target. Blockable by <hl id="captain-color">Captain</hl> or <hl id="ambassador-color">Ambassador</hl>. Can block <hl id="captain-color">STEAL</hl></p>
+                    <h3>Assassin</h3>
+                    <p><b id="assassin-color">ASSASSINATE</b>: Pay 3 coins to choose a target to assassinate (target loses an influence). Blockable by <hl id="contessa-color">Contessa</hl>.</p>
+                    <h3>Duke</h3>
+                    <p><b id="duke-color">TAX</b>: Collect 3 coins from the treasury. Not blockable. Can block Foreign Aid.</p>
+                    <h3>Ambassador</h3>
+                    <p><b id="ambassador-color">EXCHANGE</b>: Draw 2 influences into your hand and pick any 2 influences to put back. Not blockable. Can block <hl id="captain-color">STEAL</hl></p>
+                    <h3>Contessa</h3>
+                    <p><b id="contessa-color">BLOCK ASSASSINATION</b>: Can block <b id="assassin-color">assassinations</b>. Not blockable.</p>
+                    <h3>Other Actions</h3>
+                    <p><b>INCOME</b>: Collect 1 coins from the treasury.</p>
+                    <p><b>FOREIGN AID</b>: Collect 2 coins from the treasury. Blockable by <hl id="duke-color">Duke</hl>.</p>
+                    <p><b>COUP</b>: Pay 7 coins and choose a target to lose an influence. If a player starts their turn with 10 coins, they must Coup. Not Blockable.</p>
                 </div>
             </div>
             </ReactModal>

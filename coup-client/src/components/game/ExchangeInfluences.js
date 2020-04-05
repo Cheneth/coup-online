85a2cd92522887611fb277148110c88ccbb1f063
Ethan <ethan190c@gmail.com>
2020-04-05 00:24:37 -0400

    fixed bug where ambassador would revive influences

--- coup-client/src/components/game/ExchangeInfluences.js
+++ coup-client/src/components/game/ExchangeInfluences.js
@@ -7,19 +7,20 @@ export default class ExchangeInfluences extends Component {
         console.log(props.influences)
         this.state = {
              influences: props.influences,
-             putBack: []
+             keep: [],
+             totalInf: props.influences.length
         }
     }
     
 
     selectInfluence = (index) => {
         // res.revealedCard, prevaction, counterAction, challengee, challenger, isBlock
-        this.state.putBack.push(this.state.influences.splice(index,1)[0])
+        this.state.keep.push(this.state.influences.splice(index,1)[0])
         this.setState({ influences: this.state.influences, putBack: this.state.putBack })
-        if(this.state.putBack.length === 2) {
+        if(this.state.keep.length === (this.state.totalInf-2)) {
             const res = {
                 playerName: this.props.name,
-                kept: this.state.putBack,
+                kept: this.state.keep,
                 putBack: this.state.influences
             }
             console.log(res);

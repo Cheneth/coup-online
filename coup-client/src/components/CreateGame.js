9988bc6051f4c91a441b91541b9a3825416135f1
Ethan <ethan190c@gmail.com>
2020-05-31 11:59:49 -0400

    removed console

--- coup-client/src/components/CreateGame.js
+++ coup-client/src/components/CreateGame.js
@@ -6,8 +6,6 @@ import Coup from './game/Coup';
 const axios = require('axios');
 const baseUrl = process.env.REACT_APP_BACKEND_URL || "http://localhost:8000"
 
-// const baseUrl = "https://rocky-stream-49978.herokuapp.com"
-
 export default class CreateGame extends Component {
 
     constructor(props) {

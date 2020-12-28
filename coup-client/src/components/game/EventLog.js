567a9477351528387887c193a708f333792f77ab
Ethan <ethan190c@gmail.com>
2020-12-27 20:26:05 -0500

    ensure "event log" title doesn't scroll

--- coup-client/src/components/game/EventLog.js
+++ coup-client/src/components/game/EventLog.js
@@ -5,15 +5,17 @@ export default class EventLog extends Component {
     render() {
         return (
             <div className="EventLogContainer">
-                <p className="bold">Event Log</p>
-                {this.props.logs.map((x, index) => {
-                    if(index === this.props.logs.length-1){
-                        return <p className="new">{x}</p>
-                    }
+                <p className="bold EventLogTitle">Event Log</p>
+                <div className="EventLogBody">
+                   {this.props.logs.map((x, index) => {
+                        if(index === this.props.logs.length-1){
+                            return <p className="new">{x}</p>
+                        }
                     return <p>{x}</p>
-                })}
-                <div style={{ float:"left", clear: "both" }}
-                    ref={(el) => { this.messagesEnd = el; }}>
+                    })}
+                    <div style={{ float:"left", clear: "both" }}
+                        ref={(el) => { this.messagesEnd = el; }}>
+                    </div> 
                 </div>
             </div>
         )
